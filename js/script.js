62eb1c0f06225482d264be9f86d4bd22c4f3d29a
LlordZombie <linus.freistetter@hotmail.com>
2024-02-11 18:22:46 +0100

    (fast) zum ziel gepfuscht

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", main);
 function main() {
-    let x = document.getElementsByClassName("armor_picker");
-    for (let x1 of x) {
-        try {
-            x1.addEventListener("click", addArmor);
-            alert(x1.getAttributeNames());
+    const x = document.querySelectorAll(".armor_picker");
+    x.forEach(x1 => {
+        x1.addEventListener("click", addArmor);
+    });
+}
+function addArmor(event) {
+    let img = event.target.cloneNode(true);
+    let target = document.getElementById("picked");
+    let existingTargetKids = target.childNodes;
+    let exists = false;
+    let id = "target" + event.target.parentNode.id;
+    img.id = id;
+    img.addEventListener("click", function () {
+        if (img.parentNode.id === "picked") {
+            target.removeChild(img);
         }
-        catch (e) {
-            alert("du cock");
+    });
+    for (let i = 0; i < existingTargetKids.length; i++) {
+        if (existingTargetKids.item(i)?.id === id) {
+            exists = true;
         }
     }
+    if (!exists) {
+        alert(exists);
+        target.appendChild(img);
+    }
+    else {
+        alert(exists);
+        target.replaceChild(img, document.getElementById(id));
+    }
 }
-function addArmor() { }
 //# sourceMappingURL=script.js.map
--- ts/script.ts
+++ ts/script.ts
@@ -1,14 +1,36 @@
-document.addEventListener("DOMContentLoaded",main);
+document.addEventListener("DOMContentLoaded", main);
 
-function main(){
-   let x = document.getElementsByClassName("armor_picker")
-    for (let x1 of x) {
-        try{
+function main() {
+    const x: NodeListOf<HTMLElement> | null = document.querySelectorAll(".armor_picker");
+    x.forEach(x1=>{
         x1.addEventListener("click",addArmor)
-        alert(x1.getAttributeNames())
-    }catch (e) {
-            alert("du cock")
-        }}
+    })
 }
 
-function addArmor(){}
+function addArmor(event:MouseEvent) {
+    let img = (event!.target as HTMLElement).cloneNode(true);
+    let target = document.getElementById("picked")
+    let existingTargetKids = target!.childNodes;
+    let exists = false;
+    let id = "target"+((event.target as HTMLElement)!.parentNode as HTMLElement).id;
+    (img as HTMLElement).id = id;
+    img.addEventListener("click",function (){
+        if ((img.parentNode as HTMLElement).id==="picked"){
+            target!.removeChild(img);
+        }
+    })
+    for (let i = 0; i < existingTargetKids.length; i++) {
+        if ((existingTargetKids.item(i) as Element)?.id === id){
+            exists=true;
+        }
+    }
+    if (!exists){
+        alert(exists);
+        target!.appendChild(img);
+    }else{
+        alert(exists)
+        target!.replaceChild(img,(document.getElementById(id) as Node))
+    }
+
+}
+
