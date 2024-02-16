af1dc4b27c4674a4ac3833110c3d89bf5e3b2934
LlordZombie <linus.freistetter@hotmail.com>
2024-02-16 17:08:22 +0100

    finishec pimping

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", main);
+
 function main() {
     const x = document.querySelectorAll(".armor_picker");
     x.forEach(x1 => {
         x1.addEventListener("click", addArmor);
     });
 }
+
 function addArmor(event) {
     let img = event.target.cloneNode(true);
     let target = document.getElementById("picked");
     let existingTargetKids = target.childNodes;
     let exists = false;
-    let id = "target" + event.target.parentNode.id;
+    let id = event.target.parentNode.id + "target";
+    let order = "hakb";
     img.id = id;
     img.addEventListener("click", function () {
         if (img.parentNode.id === "picked") {
@@ -23,13 +26,20 @@ function addArmor(event) {
             exists = true;
         }
     }
+    img.removeAttribute("class");
+    let orderNum = (order.indexOf(img.id.substring(0, 1)));
+    img.setAttribute("order", orderNum.toString());
+    orderNum++;
+    let followingElement = document.querySelector(`[order="${orderNum.toString()}"]`);
     if (!exists) {
-        alert(exists);
-        target.appendChild(img);
-    }
-    else {
-        alert(exists);
+        if (typeof followingElement != null) {
+            target.insertBefore(img, followingElement);
+        } else {
+            target.appendChild(img);
+        }
+    } else {
         target.replaceChild(img, document.getElementById(id));
     }
 }
+
 //# sourceMappingURL=script.js.map
--- ts/script.ts
+++ ts/script.ts
@@ -1,33 +1,43 @@
 document.addEventListener("DOMContentLoaded", main);
-function main() {
+
+function main(): void {
     const x: NodeListOf<HTMLElement> | null = document.querySelectorAll(".armor_picker");
-    x.forEach(x1=>{
-        x1.addEventListener("click",addArmor)
+    x.forEach(x1 => {
+        x1.addEventListener("click", addArmor);
     })
 }
-function addArmor(event:MouseEvent) {
-    let img = (event!.target as HTMLElement).cloneNode(true);
-    let target = document.getElementById("picked")
-    let existingTargetKids = target!.childNodes;
-    let exists = false;
-    let id = "target"+((event.target as HTMLElement)!.parentNode as HTMLElement).id;
+
+function addArmor(event: MouseEvent): void {
+    let img: Node = (event!.target as HTMLElement).cloneNode(true);
+    let target: HTMLElement | null = document.getElementById("picked");
+    let existingTargetKids: NodeListOf<ChildNode> = target!.childNodes;
+    let exists: boolean = false;
+    let id: string = ((event.target as HTMLElement)!.parentNode as HTMLElement).id + "target";
+    let order: string = "hakb";
     (img as HTMLElement).id = id;
-    img.addEventListener("click",function (){
-        if ((img.parentNode as HTMLElement).id==="picked"){
+    img.addEventListener("click", function (): void {
+        if ((img.parentNode as HTMLElement).id === "picked") {
             target!.removeChild(img);
         }
-    })
-    for (let i = 0; i < existingTargetKids.length; i++) {
-        if ((existingTargetKids.item(i) as Element)?.id === id){
-            exists=true;
+    });
+    for (let i: number = 0; i < existingTargetKids.length; i++) {
+        if ((existingTargetKids.item(i) as Element)?.id === id) {
+            exists = true;
         }
     }
-    if (!exists){
-        alert(exists);
-        target!.appendChild(img);
-    }else{
-        alert(exists)
-        target!.replaceChild(img,(document.getElementById(id) as Node))
+    (img as HTMLElement).removeAttribute("class");
+    let orderNum = (order.indexOf((img as HTMLElement).id.substring(0, 1)));
+    (img as HTMLElement).setAttribute("order", orderNum.toString());
+    orderNum++;
+    let followingElement = document.querySelector(`[order="${orderNum.toString()}"]`)
+    if (!exists) {
+        if (typeof followingElement != null) {
+            target!.insertBefore(img, (followingElement as Node))
+        } else {
+            target!.appendChild(img);
+        }
+    } else {
+        target!.replaceChild(img, (document.getElementById(id) as Node));
     }
 
 }
